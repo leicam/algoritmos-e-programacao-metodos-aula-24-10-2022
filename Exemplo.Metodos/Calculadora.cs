9e9433cac36d8e26ecd999c6a26207aa35ab594d
Juliano Ribeiro de Souza Maciel <juliano.maciel@faculdadefacec.edu.br>
2022-10-24 20:22:56 -0300

    Implementações em sala

---
+++ Exemplo.Metodos/Calculadora.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exemplo.Metodos
+{
+    /// <summary>
+    /// Somar(param1, param2) e Somar(param1, param2, param3)
+    /// estes dois métodos caracterizam uma sobrecarga
+    /// </summary>
+    public class Calculadora
+    {
+        public static decimal Somar(decimal valorUm,
+            decimal valorDois)
+        {
+            return valorUm + valorDois;
+        }
+
+        //não é possivel trocar o tipo de retorno de um método
+        //ao criar uma sobrecarga
+        //public static void Somar(decimal valorUm, decimal valorDois)
+        //{
+
+        //}
+
+        public static decimal Somar(decimal valorUm,
+            decimal valorDois, decimal valorTres)
+        {
+            decimal resultado = valorUm + valorDois + valorTres;
+
+            return resultado;
+        }
+
+        //metodo com parâmetro valorTres não obrigatorio
+        public static decimal Subtrair(decimal valorUm,
+            decimal valorDois, decimal valorTres = 0.0m)
+        {
+            if (valorUm <= 0.00m)
+                throw new ArgumentException("Variavel valorUm inválida!");
+
+            decimal resultado = valorUm - valorDois - valorTres;
+
+            return resultado;
+        }
+    }
+}
---
+++ Exemplo.Metodos/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exemplo.Metodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal somaUm = Calculadora
+                .Somar(15.15m, 12.13m);
+
+            Console.WriteLine("Soma um: " + somaUm);
+
+            decimal somaDois = Calculadora
+                .Somar(11.23m, 13.26m, 33.63m);
+
+            Console.WriteLine("Soma dois: " + somaDois);
+
+            Console.WriteLine("Subtração: "
+                + Calculadora.Subtrair(15.23m, 12.23m));
+
+            Console.WriteLine("Subtração: "
+                + Calculadora.Subtrair(15.23m, 12.23m, 1.00m));
+
+            Console.WriteLine("Subtração: "
+                + Calculadora.Subtrair(15.23m, -12.23m, -10.00m));
+
+            Console.ReadKey();
+        }
+    }
+}
